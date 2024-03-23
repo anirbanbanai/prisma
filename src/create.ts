683253c6c7147d34a851b5fcd928a724cfda76c8
Anirban Banai <anirbanchondrobanai@gmail.com>
2024-03-23 10:59:13 +0600

    updated

--- src/create.ts
+++ src/create.ts
@@ -1,45 +1,81 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, UserRole } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const main =async ()=>{
-// const result = await prisma.post.create({
-//     data: {
-//         title:"this is title...2",
-//         content:"this is content...31",
-//         authorName:"Amir..3j"
-//     }
-// });
+const main = async () => {
+  //   const createUser = await prisma.user.create({
+  //     data: {
+  //       username: "user2",
+  //       email:'user2@gmail.com',
+  //       role:UserRole.user,
+  //     },
+  //   });
 
-// console.log(result);
+  //   const createProfile = await prisma.profile.create({
+  //     data:{
+  //         bio:"this is bio",
+  //         userId: 1
+  //     }
+  //   })
 
-const post = await prisma.post.createMany({
-    data:[
-        {
-            title:"title-1",
-            content:"content-1",
-            authorName:"author1"
-        },
-        {
-            title:"title-2",
-            content:"content-2",
-            authorName:"author2"
-        },
-        {
-            title:"title-3",
-            content:"content-3",
-            authorName:"author3"
-        },
-        {
-            title:"title-4",
-            content:"content-4",
-            authorName:"author4"
-        },
-    ]
-});
+  // const createCategory = await prisma.category.create({
+  //     data:{
+  //         name:"Tech dev"
+  //     }
+  // })
 
-console.log(post);
+  const createPost = await prisma.post.create({
+    data: {
+      title: "This is title 55",
+      content: "this is content the post 55",
+      authorId: 3,
+      postCategory: {
+        create:[
+            {
+                categoryId:1
+            },
+            {
+                categoryId:3
+            },
+            {
+                categoryId:4
+            },
+        ]
+      },
+    },
+    include:{
+        postCategory: true
+    }
+  });
 
-}
- 
-main()
+  console.log(createPost);
+
+  // const post = await prisma.post.createMany({
+  //     data:[
+  //         {
+  //             title:"title-1",
+  //             content:"content-1",
+  //             authorName:"author1"
+  //         },
+  //         {
+  //             title:"title-2",
+  //             content:"content-2",
+  //             authorName:"author2"
+  //         },
+  //         {
+  //             title:"title-3",
+  //             content:"content-3",
+  //             authorName:"author3"
+  //         },
+  //         {
+  //             title:"title-4",
+  //             content:"content-4",
+  //             authorName:"author4"
+  //         },
+  //     ]
+  // });
+
+  // console.log(post);
+};
+
+main();
--- src/pagination-sorting.ts
+++ src/pagination-sorting.ts
@@ -2,36 +2,56 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const paginationSroting =async()=>{
-// offset base pagination
-    // const offSetData = await prisma.post.findMany({
-    //     skip:5,
-    //     take:4                    
-    // })
+const paginationSroting = async () => {
+  // offset base pagination
+  // const offSetData = await prisma.post.findMany({
+  //     skip:5,
+  //     take:4
+  // })
 
-    // cursor base pagination
-    // const offSetData = await prisma.post.findMany({
-    //     skip:1,
-    //     take:2  ,
-    //     cursor:{
-    //         id:18
-    //     }                  
-    // })
+  // cursor base pagination
+  // const offSetData = await prisma.post.findMany({
+  //     skip:1,
+  //     take:2  ,
+  //     cursor:{
+  //         id:18
+  //     }
+  // })
 
-    // console.log("offset pagination data", offSetData);
+  // console.log("offset pagination data", offSetData);
 
-    // assc and desc order
-    const sortingData = await prisma.post.findMany({
-        orderBy: {
-            title:"desc"
+  // assc and desc order
+  // const sortingData = await prisma.post.findMany({
+  //     orderBy: {
+  //         title:"desc"
+  //     },
+  //     where:{
+  //         title:'title-1'
+  //     }
+  // });
+
+  // fluent api
+  // const result  = await prisma.user.findUnique({
+  //     where:{
+  //         id:1
+  //     },
+
+  // }).profile()
+
+  // relational flter
+  const publishedPostUsers = await prisma.user
+    .findMany({
+      include: {
+        post: {
+          where: {
+            published: true
+          },
         },
-        where:{
-            title:'title-1'
-        }
-    });
+      },
+    })
+    
 
-    console.log("sorted data", sortingData);
+  console.log( publishedPostUsers ,{depth: Infinity});
+};
 
-}
-
-paginationSroting()
+paginationSroting();
